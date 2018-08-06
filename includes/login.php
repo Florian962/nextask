c60a4bfdfd445fc3f980cf95e211743709828d14
Florian Raeymaekers <r0676161@student.thomasmore.be>
2018-08-06 16:35:05 +0200

    create nav

---
+++ includes/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="shortcut icon" href="Images/logo.png">
+    <link rel="stylesheet" href="../assets/css/normalize.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+
+    <title>NEXTASK | Login</title>
+</head>
+<body>
+
+    <nav>
+        <ul class="nav__main">	
+            <li class="nav__main--logo"><a href="#"><img src="../assets/images/logo.png" alt=""></a></li>
+            <li class="nav__main--link nav__main--link1"><a href="#">Enterprise</a></li>
+            <li class="nav__main--link nav__main--link2"><a href="#">New features</a></li>
+            <li class="nav__main--link nav__main--link3"><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+
+    
+
+    <footer>
+    
+    </footer>
+</body>
+</html>
