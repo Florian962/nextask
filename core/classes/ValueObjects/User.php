060a8ed303fdc77efaf6f97b1e89c404a8c31950
Florian Raeymaekers <r0676161@student.thomasmore.be>
2018-08-25 09:56:34 +0200

    User properties in ValueObjects

---
+++ core/classes/ValueObjects/User.php
@@ -0,0 +1,111 @@
+<?php
+
+class User {
+
+    /* In deze class zitten alle properties voor de authenticion */
+    protected $user_id;
+    protected $username;
+    protected $email;
+    protected $password;
+    protected $loggedIn = false;
+
+    /**
+     * Get the value of user_id
+     */ 
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Set the value of user_id
+     *
+     * @return  self
+     */ 
+    public function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of username
+     */ 
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set the value of username
+     *
+     * @return  self
+     */ 
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */ 
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /* zet logged in op waar */
+    public function setLoggedIn()
+    {
+        $this->loggedIn = true;
+    }
+    /* zet logged in op niet waar */
+    public function setLoggedOut()
+    {
+        $this->loggedIn = false;;
+
+        return $this;
+    }
+
+    public function fetchUserFromDb($fetchData) {
+        $this->setUserId($fetchData['user_id']);
+        $this->setUsername($fetchData['username']);
+        $this->setEmail($fetchData['email']);
+        $this->setPassword($fetchData['password']);
+    }
+}
