3ac1bf9ce5649c5ef40c3b8eab61bf774d4f1799
Florian Raeymaekers <r0676161@student.thomasmore.be>
2018-08-25 12:01:04 +0200

    add functions file for functions

---
+++ core/functions/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+        /* Function that checks input field. */
+        function checkInput($var) {
+            $var = htmlspecialchars($var);
+            $var = trim($var);
+            $var = stripcslashes($var);
+            return $var;
+        }
+
