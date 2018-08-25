03e68c51c1d1d0cdf44f3355d11ec4d56d13b79e
Florian Raeymaekers <r0676161@student.thomasmore.be>
2018-08-25 09:42:16 +0200

    include database class in init file

--- core/init.php
+++ core/init.php
@@ -1,22 +1,35 @@
 <?php
+    /* db connection */
+    include_once 'database/Database.php';
+
+    /*
     include 'database/connection.php';
     include 'classes/user.php';
     include 'classes/list.php';
     include 'classes/task.php';
     include 'classes/comment.php';
     include 'classes/admin.php';
+    */
 
     /* om aan de conn te kunnen uit connection.php */
+    /*
     global $pdo;
+    */
+
+    Database::getInstance();
+    //var_dump(Database::getInstance());
 
     session_start();
 
-    $getFromU = new User($pdo);
+    $as = new AuthenticationService();
+    /* get user data */
+    
+
+    /*$getFromU = new User($pdo);
     $getFromL = new Todolist($pdo);
     $getFromT = new Task($pdo);
     $getFromC = new Comment($pdo);
-    $getFromA = new Admin($pdo);
+    $getFromA = new Admin($pdo);*/
 
     define("BASE_URL", "http://localhost/nextask/");
     //define("BASE_URL", "http://nextask.florianraeymaekers.be/");
-?>
