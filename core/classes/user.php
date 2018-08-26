f48ead387c2d82ef3cbf91fea6db4823a39f7b2c
Florian Raeymaekers <r0676161@student.thomasmore.be>
2018-08-26 09:56:56 +0200

    Functions moved to other files, change of the db conn stmt

--- core/classes/user.php
+++ core/classes/user.php
@@ -1,53 +1,20 @@
 <?php
+    require_once 'Authentication.php';
+
     class User {
         /* nu kunnen alle klassen ook aan deze PDO die deze klasse extenden */
-        protected $pdo;
+        protected $db;
 
         /* zo kan deze klasse aan de db. */
-        function __construct($pdo){
-            $this->pdo = $pdo;
+        public function __construct() {
+            $this->db = Database::getInstance();
         }
 
-        /* Function that checks input field. */
-        public function checkInput($var) {
-            $var = htmlspecialchars($var);
-            $var = trim($var);
-            $var = stripcslashes($var);
-            return $var;
-        }
 
-        /* Function to login. */
-        public function login($email, $password) {
-            
-            $stmt = $this->pdo->prepare("SELECT * FROM `users` WHERE `email` = :email");
-            $stmt->bindParam(":email", $email);
-            $result = $stmt->execute();
-            $checkUser = $stmt->fetch(PDO::FETCH_ASSOC);
-            //var_dump($checkUser);
-            //var_dump($password);
-            if(!empty($checkUser)){
-                //var_dump(password_verify($password, $checkUser['password']));
-                if(password_verify($password, $checkUser['password']) ){
-                    $_SESSION['user_id'] = $checkUser['user_id'];
-                    var_dump($_SESSION['user_id']);
-                    header('Location: ../index.php');    
-                }
-                else {
-                    return false;
-                }    
-            }
-        }
-
-        /* Function to logout. */
-        public function logout() {
-            $_SESSION = array();
-            session_destroy();
-            header('Location: ../../php/welcome.php');
-        }
 
         /* Function that checks if email is already in db. */
         public function checkEmail ($email) {
-            $stmt = $this->pdo->prepare("SELECT `email` FROM `users` WHERE `email` = :email");
+            $stmt = $this->db->getPDO()->prepare("SELECT `email` FROM `users` WHERE `email` = :email");
             $stmt->bindParam(":email", $email, PDO::PARAM_STR);
             $stmt->execute();
 
@@ -60,15 +27,9 @@
             }
         }
 
-        /* Function that checks if user is logged in or not. */
-        public function loggedIn () {
-            /* als er een session is returnt de functie true en anders fout. */
-            return (isset($_SESSION['user_id'])) ? true : false;
-        }
-
         /* Function that returns user data.  */
         public function userData($user_id) {
-            $stmt = $this->pdo->prepare("SELECT * FROM `users` WHERE `user_id` = :user_id");
+            $stmt = $this->db->getPDO()->prepare("SELECT * FROM `users` WHERE `user_id` = :user_id");
             $stmt->bindParam(":user_id", $user_id, PDO::PARAM_INT);
             $stmt->execute();
         
@@ -77,14 +38,14 @@
 
         /* Function to register. */
         public function register ($username, $email, $hash) {
-            $stmt = $this->pdo->prepare("INSERT INTO `users` (`username`, `email`, `password`) VALUES (:username, :email, :password)");
+            $stmt = $this->db->getPDO()->prepare("INSERT INTO `users` (`username`, `email`, `password`) VALUES (:username, :email, :password)");
             $stmt->bindParam(":username", $username, PDO::PARAM_STR);
             $stmt->bindParam(":email", $email, PDO::PARAM_STR);
             $stmt->bindParam(":password", $hash, PDO::PARAM_STR);
             $stmt->execute();
 
             /* Geeft laatst ingevoerde user id. */
-            $user_id = $this->pdo->lastInsertId();
+            $user_id = $this->db->getPDO()->lastInsertId();
             $_SESSION['user_id'] = $user_id;
         }
 
