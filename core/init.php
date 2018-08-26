23eb815a1971f5e022841f23c1a5d8483eb405b5
Florian Raeymaekers <r0676161@student.thomasmore.be>
2018-08-26 09:52:03 +0200

    Remove global pdo and remove all classes in  file.
    dit is beter omdat je nu niet meer telkens alle classes moet laden bij het laden van een pagina

--- core/init.php
+++ core/init.php
@@ -1,21 +1,32 @@
 <?php
-    include 'database/connection.php';
-    include 'classes/user.php';
+    /*include 'database/connection.php';
+    
     include 'classes/list.php';
     include 'classes/task.php';
     include 'classes/comment.php';
-    include 'classes/admin.php';
+    include 'classes/admin.php';*/
+    include 'database/Database.php';
+    include 'classes/Authentication.php';
+    include 'functions/functions.php';
+
+    Database::getInstance();
 
     /* om aan de conn te kunnen uit connection.php */
-    global $pdo;
+    //global $pdo;
 
     session_start();
 
-    $getFromU = new User($pdo);
+    $as = new Authentication();
+
+    if($as->loggedIn() === false && $_SERVER['REQUEST_URI'] !== '/nextask/php/welcome.php'){
+        header('Location: http://localhost/nextask/php/welcome.php');
+    }
+
+    /*$getFromU = new User($pdo);
     $getFromL = new Todolist($pdo);
     $getFromT = new Task($pdo);
     $getFromC = new Comment($pdo);
-    $getFromA = new Admin($pdo);
+    $getFromA = new Admin($pdo);*/
 
     define("BASE_URL", "http://localhost/nextask/");
     //define("BASE_URL", "http://nextask.florianraeymaekers.be/");
