34e76d6f4ab8f408b72c048554a73be246885788
Florian Raeymaekers <r0676161@student.thomasmore.be>
2018-08-26 19:28:14 +0200

    remove unused classes

--- core/init.php
+++ core/init.php
@@ -1,10 +1,5 @@
 <?php
-    /*include 'database/connection.php';
-    
-    include 'classes/list.php';
-    include 'classes/task.php';
-    include 'classes/comment.php';
-    include 'classes/admin.php';*/
+
     include 'database/Database.php';
     include 'classes/Authentication.php';
     include 'functions/functions.php';
@@ -21,12 +16,6 @@
     if($as->loggedIn() === false && $_SERVER['REQUEST_URI'] !== '/nextask/php/welcome.php'){
         header('Location: http://localhost/nextask/php/welcome.php');
     }
-
-    /*$getFromU = new User($pdo);
-    $getFromL = new Todolist($pdo);
-    $getFromT = new Task($pdo);
-    $getFromC = new Comment($pdo);
-    $getFromA = new Admin($pdo);*/
 
     define("BASE_URL", "http://localhost/nextask/");
     //define("BASE_URL", "http://nextask.florianraeymaekers.be/");
